a87cff1282b2ce97ab6922fe3d9bba39303282b7
Corey Collins <coreycollinsm@gmail.com>
2022-07-07 17:16:21 -0400

    Updated Certificates Section
    - Added multiple certificates.
    - Added complete parameter to cert objects.
    - Added ::before with (ongoing) message for coursework not complete

--- src/data/certs.js
+++ src/data/certs.js
@@ -1,9 +1,27 @@
 const certsArray = [
     {
         id: 1,
+        title: "2022 Web Dev Bootcamp",
+        company: "Udemy",
+        author: "Dr. Angela Yu",
+        date: "July 2022",
+        complete: false,
+        bullets: [
+            "DOM Manipulation, JQuery and Backend Web Development",
+            "Decentralised Apps, Deployment, Crypto Tokens & NFTs",
+            "RESTful APIs, Authentication & Security, ReactJS",
+            "HTML, CSS, Bootstrap, JavaScript ES6 Review",
+            "Databases: SQL, MongoDB, Mongoose",
+            "NodeJS, ExpressJS, and APIs",
+        ]
+    },
+    {
+        id: 2,
         title: "Mastering React",
         company: "CWM",
+        author: "Mosh Hamedani",
         date: "July 2022",
+        complete: true,
         bullets: [
             "Authentication, authorization, deployment and advanced topics",
             "Routing, forms, and calling back end services",
@@ -12,10 +30,12 @@ const certsArray = [
         ]
     },
     {
-        id: 2,
+        id: 3,
         title: "JavaScript Mastery II",
         company: "CWM",
+        author: "Mosh Hamedani",
         date: "May 2022",
+        complete: true,
         bullets: [
             "Prototypical inheritance (in depth: method overriding & polymorphism)",
             "Inheritance, prototypes, and prototypical inheritance",
@@ -25,10 +45,12 @@ const certsArray = [
         ]
     },
     {
-        id: 3,
+        id: 4,
         title: "JavaScript Mastery I",
         company: "CWM",
+        author: "Mosh Hamedani",
         date: "April 2022",
+        complete: true,
         bullets: [
             "Objects, functions, arrays and common methods (i.e. reduce & map)",
             "Error handling, clean code, refactoring, and .this",
@@ -38,10 +60,40 @@ const certsArray = [
         ]
     },
     {
-        id: 4,
+        id: 5,
+        title: "Complete JavaScript 2022",
+        company: "Udemy",
+        author: "Jonas Schmedtmann",
+        date: "April 2022",
+        complete: true,
+        bullets: [
+            "Asynchronous JS: Promises, Async/Await, and AJAX",
+            "Object Oriented Programming in JavaScript",
+            "JavaScript, DOM, and events fundamentals",
+            "Data Structures, Functions and Arrays",
+        ]
+    },
+    {
+        id: 6,
+        title: "Responsive HTML & CSS",
+        company: "Udemy",
+        author: "Jonas Schmedtmann",
+        date: "April 2022",
+        complete: true,
+        bullets: [
+            "Web Design rules, freamework & layout patterns",
+            "Layouts, Floats, Flexbox & Grid Fundamentals",
+            "Classes & control flow, loops & operators",
+            "HTML & CSS Fundamentals",
+        ]
+    },
+    {
+        id: 7,
         title: "Java Fundamentals",
         company: "CWM",
+        author: "Mosh Hamedani",
         date: "December 2020",
+        complete: true,
         bullets: [
             "Sequences, arrays, expressions, objects, and variables",
             "Clean code etiquette and debugging/deployment",
--- src/pages/certificates/components/certsComponent/certsComponent.jsx
+++ src/pages/certificates/components/certsComponent/certsComponent.jsx
@@ -3,7 +3,11 @@ import './certsComponent.css';
 import Certs from '../../../../data/certs';
 
 class CertsComponent extends Component {
-    state = { certs: Certs } 
+    state = { certs: Certs }
+
+    isComplete(cert) {
+        if (!cert) return "ongoing";
+    }
 
     render() { 
         return (
@@ -11,9 +15,10 @@ class CertsComponent extends Component {
                 {this.state.certs.map( cert => {
                     return (
                         <div className="cert" key={cert.id}>
-                            <div className="cert-header">
+                            <div className={"cert-header " + this.isComplete(cert.complete)}>
                                 <h1><span className="blue-text">{cert.title}</span> <span className="thin-text">from</span> {cert.company}</h1>
-                                <h2>{cert.date}</h2>
+                                <h2>{cert.author}</h2>
+                                <h3>{cert.date}</h3>
                             </div>
                             <ul>
                                 {cert.bullets.map( bullet => {
