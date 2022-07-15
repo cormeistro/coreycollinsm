4cac2b38a59c44ab8405d55c376f576e0f196c38
Corey Collins <coreycollinsm@gmail.com>
2022-07-14 21:32:48 -0400

    Updated Social Links

--- src/links.js
+++ src/links.js
@@ -1,4 +1,4 @@
-export const githubURL = "https://github.com/coreycollinsm";
-export const instagramURL = "http://instagram.com/coreycollinsm";
+export const githubURL = "https://github.com/cormeistro";
+export const instagramURL = "http://instagram.com/cormeistro";
 export const linkedInURL = "https://www.linkedin.com/in/corey-collins-925314154/";
-export const twitterURL = "http://twitter.com/coreycollinsm";
+export const twitterURL = "http://twitter.com/cormeistro";
